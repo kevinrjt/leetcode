c9f2cc3d0642f53ecbb764532f21c9d66cf4eac1
Juntao Ren <kevinrjt@163.com>
2020-11-21 19:01:17 +0800

    Hello

---
+++ definitions.h
@@ -0,0 +1,55 @@
+#ifndef DEFINITIONS_H_
+#define DEFINITIONS_H_
+
+#include <vector>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// Definition for a Node.
+class Node
+{
+public:
+    int val;
+    vector<Node *> neighbors;
+
+    Node()
+    {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+
+    Node(int _val)
+    {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+
+    Node(int _val, vector<Node *> _neighbors)
+    {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+#endif // DEFINITIONS_H_
---
+++ hello.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "definitions.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, LeetCode!" << endl;
+
+    ListNode listNode;
+    cout << listNode.val << endl;
+
+    TreeNode treeNode;
+    cout << treeNode.val << endl;
+
+    Node node;
+    cout << node.val << endl;
+
+    return 0;
+}
