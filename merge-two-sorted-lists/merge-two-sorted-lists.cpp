ff3410beaad420e9c952242e1abd0952aab11394
Juntao Ren <kevinrjt@163.com>
2020-11-22 20:00:34 +0800

    Solve merge-two-sorted-lists

---
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "definitions.h"
+
+using namespace std;
+
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+{
+    ListNode dummy = ListNode();
+    ListNode *node = &dummy;
+    while (l1 != nullptr && l2 != nullptr)
+    {
+        if (l1->val <= l2->val)
+        {
+            node->next = l1;
+            l1 = l1->next;
+        }
+        else
+        {
+            node->next = l2;
+            l2 = l2->next;
+        }
+
+        node = node->next;
+    }
+
+    node->next = l1 != nullptr ? l1 : l2;
+    return dummy.next;
+}
+
+int main()
+{
+    return 0;
+}
