398e8ac3d8a354d406e8b1137f92b557fd0fd80d
Juntao Ren <kevinrjt@163.com>
2020-11-22 19:31:56 +0800

    Solve remove-nth-node-from-end-of-list

---
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include "definitions.h"
+
+using namespace std;
+
+ListNode *removeNthFromEnd(ListNode *head, int n)
+{
+    ListNode dummy = ListNode(0, head);
+    ListNode *slow = &dummy;
+    ListNode *fast = slow;
+    for (int i = 0; i < n; ++i)
+    {
+        fast = fast->next;
+    }
+
+    while (fast->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    ListNode *nthFromEnd = slow->next;
+    slow->next = nthFromEnd->next;
+    delete nthFromEnd;
+    return dummy.next;
+}
+
+int main()
+{
+    return 0;
+}
