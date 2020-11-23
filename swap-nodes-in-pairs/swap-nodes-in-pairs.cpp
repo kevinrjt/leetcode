78a3d11e89d7701cf6921e05451e73eda50699fb
Juntao Ren <kevinrjt@163.com>
2020-11-23 09:02:41 +0800

    Solve swap-nodes-in-pairs

---
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "definitions.h"
+
+using namespace std;
+
+ListNode *swapPairs(ListNode *head)
+{
+    ListNode dummy = ListNode(0, head);
+    ListNode *node = &dummy;
+    while (node->next != nullptr && node->next->next != nullptr)
+    {
+        ListNode *temp = node->next;
+        node->next = temp->next;
+        temp->next = node->next->next;
+        node->next->next = temp;
+
+        node = temp;
+    }
+
+    return dummy.next;
+}
+
+int main()
+{
+    return 0;
+}
