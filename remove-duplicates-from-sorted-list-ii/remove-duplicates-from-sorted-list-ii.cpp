0f58c7ff8efc8d1bcf77b898e2ed7d6c443dea52
Juntao Ren <kevinrjt@163.com>
2020-11-24 19:59:37 +0800

    Solve remove-duplicates-from-sorted-list-ii

---
+++ remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "../definitions.h"
+
+using namespace std;
+
+ListNode *deleteDuplicates(ListNode *head)
+{
+    ListNode **pNode = &head;
+    while (*pNode != nullptr)
+    {
+        ListNode *node = *pNode;
+        if (node->next != nullptr && node->val == node->next->val)
+        {
+            int val = node->val;
+            while (node != nullptr && node->val == val)
+            {
+                ListNode *temp = node;
+                node = node->next;
+                delete temp;
+            }
+
+            *pNode = node;
+        }
+        else
+        {
+            pNode = &(node->next);
+        }
+    }
+
+    return head;
+}
+
+int main()
+{
+    return 0;
+}
