5573340436e5ffb4682470a2a4716cf218148199
Juntao Ren <kevinrjt@163.com>
2020-11-24 15:44:08 +0800

    Solve linked-list-cycle-ii

---
+++ linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "definitions.h"
+
+using namespace std;
+
+ListNode *detectCycle(ListNode *head)
+{
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast != nullptr && fast->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            while (slow != head)
+            {
+                slow = slow->next;
+                head = head->next;
+            }
+
+            return slow;
+        }
+    }
+
+    return nullptr;
+}
+
+int main()
+{
+    return 0;
+}
