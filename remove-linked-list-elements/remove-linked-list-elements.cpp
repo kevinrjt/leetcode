647fca57278911e06a1fbe2fb88d864d38009f9f
Juntao Ren <kevinrjt@163.com>
2020-11-24 18:24:16 +0800

    Solve remove-linked-list-elements

---
+++ remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "definitions.h"
+
+using namespace std;
+
+ListNode *removeElements(ListNode *head, int val)
+{
+    ListNode **pNode = &head;
+    while (*pNode != nullptr)
+    {
+        if ((*pNode)->val == val)
+        {
+            ListNode *temp = *pNode;
+            *pNode = (*pNode)->next;
+            delete temp;
+        }
+        else
+        {
+            pNode = &((*pNode)->next);
+        }
+    }
+    return head;
+}
+
+int main()
+{
+    return 0;
+}
