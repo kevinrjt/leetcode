8cf232d97901b7ea1790d82513d1df2add54a64d
Juntao Ren <kevinrjt@163.com>
2020-11-24 13:44:19 +0800

    Solve delete-node-in-a-linked-list

---
+++ delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include "definitions.h"
+
+using namespace std;
+
+#define SOLUTION_2
+
+#ifdef SOLUTION_1
+void deleteNode(ListNode *node)
+{
+    ListNode *temp = node->next;
+    node->val = temp->val;
+    node->next = temp->next;
+    delete temp;
+}
+#endif
+
+#ifdef SOLUTION_2
+void deleteNode(ListNode *node)
+{
+    ListNode *temp = node->next;
+    *node = *temp;
+    delete temp;
+}
+#endif
+
+int main()
+{
+    return 0;
+}
