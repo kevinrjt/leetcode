50f6c1021362992c0a067e216e18686666b85475
Juntao Ren <kevinrjt@163.com>
2020-11-25 20:51:59 +0800

    Solve rotate-list

---
+++ rotate-list/rotate-list.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "../definitions.h"
+
+using namespace std;
+
+ListNode *rotateRight(ListNode *head, int k)
+{
+    if (head == nullptr)
+    {
+        return head;
+    }
+
+    int len = 1;
+    ListNode *node = head;
+    for (; node->next != nullptr; node = node->next)
+    {
+        ++len;
+    }
+
+    node->next = head;
+    k %= len;
+    for (int i = (k == 0 ? len : k); i < len; ++i)
+    {
+        node = node->next;
+    }
+
+    head = node->next;
+    node->next = nullptr;
+    return head;
+}
+
+int main()
+{
+    return 0;
+}
