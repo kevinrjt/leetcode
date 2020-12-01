ec92bdc4b0fd1eafcee6c6de8edf0266b23e721b
Juntao Ren <kevinrjt@163.com>
2020-12-01 19:48:47 +0800

    Solve middle-of-the-linked-list

---
+++ middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "../definitions.h"
+
+using namespace std;
+
+ListNode *middleNode(ListNode *head)
+{
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast != nullptr && fast->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+int main()
+{
+    return 0;
+}
