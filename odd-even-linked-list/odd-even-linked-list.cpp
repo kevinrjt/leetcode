cb19c55eb90cb0d27443f6841b8a8fc0f277ff1d
Juntao Ren <kevinrjt@163.com>
2020-12-02 21:00:52 +0800

    Solve odd-even-linked-list

---
+++ odd-even-linked-list/odd-even-linked-list.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include "../definitions.h"
+
+using namespace std;
+
+ListNode *oddEvenList(ListNode *head)
+{
+    if (head == nullptr)
+    {
+        return nullptr;
+    }
+
+    ListNode *odd = head;
+    ListNode *even = head->next;
+    ListNode *evenHead = even;
+    while (even != nullptr && even->next != nullptr)
+    {
+        odd->next = even->next;
+        odd = odd->next;
+        even->next = odd->next;
+        even = even->next;
+    }
+
+    odd->next = evenHead;
+    return head;
+}
+
+int main()
+{
+    return 0;
+}
