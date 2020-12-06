ced08ddd436ca2b47a63554b1f917e9a8fe5c7a4
Juntao Ren <kevinrjt@163.com>
2020-12-06 19:21:38 +0800

    Add solution 2 for climbing-stairs

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,20 +1,67 @@
 #include <iostream>
+#include <utility>
 
 using namespace std;
 
+#define SOLUTION_1
+
+#ifdef SOLUTION_1
 int climbStairs(int n)
 {
+    int prevResult = 1;
     int result = 1;
-    int preResult = 1;
     for (int i = 2; i <= n; ++i)
     {
-        int temp = result;
-        result += preResult;
-        preResult = temp;
+        int nextResult = prevResult + result;
+        prevResult = result;
+        result = nextResult;
     }
 
     return result;
 }
+#endif
+
+#ifdef SOLUTION_2
+typedef pair<pair<int, int>, pair<int, int>> Matrix;
+
+int dot(const pair<int, int> &x, const pair<int, int> &y)
+{
+    return x.first * y.first + x.second * y.second;
+}
+
+Matrix multiply(const Matrix &x, const Matrix &y)
+{
+    return Matrix{{dot(x.first, y.first), dot(x.first, y.second)}, {dot(x.second, y.first), dot(x.second, y.second)}};
+}
+
+Matrix matrixPow(Matrix &x, int n)
+{
+    Matrix pow{{1, 0}, {0, 1}};
+    while (n)
+    {
+        if (n & 1)
+        {
+            pow = multiply(pow, x);
+        }
+
+        if (n > 1)
+        {
+            x = multiply(x, x);
+        }
+
+        n >>= 1;
+    }
+
+    return pow;
+}
+
+int climbStairs(int n)
+{
+    Matrix x{{1, 1}, {1, 0}};
+    Matrix pow = matrixPow(x, n);
+    return pow.first.first;
+}
+#endif
 
 int main()
 {
