5c9703af2d7f9eb60d8c49110924061b05b7551d
Juntao Ren <kevinrjt@163.com>
2020-12-12 19:54:38 +0800

    Solve longest-common-prefix

---
+++ longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string longestCommonPrefix(vector<string> &strs)
+{
+    if (strs.empty())
+    {
+        return "";
+    }
+
+    for (int j = 0; j < strs[0].size(); ++j)
+    {
+        char c = strs[0][j];
+        for (int i = 1; i < strs.size(); ++i)
+        {
+            if (j >= strs[i].size() || strs[i][j] != c)
+            {
+                return strs[0].substr(0, j);
+            }
+        }
+    }
+
+    return strs[0];
+}
+
+int main()
+{
+    return 0;
+}
