21eb19a3393cd992a115cacec38b28435320ea16
Juntao Ren <kevinrjt@163.com>
2021-01-05 21:09:02 +0800

    Solve binary-tree-level-order-traversal-ii

---
+++ binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,45 @@
+#include <stack>
+#include <vector>
+
+#include "../definitions.h"
+
+using namespace std;
+
+vector<vector<int>> levelOrderBottom(TreeNode *root)
+{
+    vector<TreeNode *> nodes;
+    if (root != nullptr)
+    {
+        nodes.push_back(root);
+    }
+
+    vector<vector<int>> levels;
+    while (!nodes.empty())
+    {
+        vector<TreeNode *> temp(move(nodes));
+        vector<int> level;
+        for (TreeNode *node : temp)
+        {
+            level.push_back(node->val);
+            if (node->left != nullptr)
+            {
+                nodes.push_back(node->left);
+            }
+
+            if (node->right != nullptr)
+            {
+                nodes.push_back(node->right);
+            }
+        }
+
+        levels.push_back(move(level));
+    }
+
+    reverse(levels.begin(), levels.end());
+    return levels;
+}
+
+int main()
+{
+    return 0;
+}
