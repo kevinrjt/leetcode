55afc66af967c1acda03b105b99a6c18eaac5710
Juntao Ren <kevinrjt@163.com>
2021-01-24 13:38:20 +0800

    Solve latest-time-by-replacing-hidden-digits

---
+++ latest-time-by-replacing-hidden-digits/latest-time-by-replacing-hidden-digits.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+using namespace std;
+
+string maximumTime(string time)
+{
+    time[0] = time[0] != '?' ? time[0] : (time[1] == '?' || time[1] <= '3' ? '2' : '1');
+    time[1] = time[1] != '?' ? time[1] : (time[0] == '2' ? '3' : '9');
+    time[3] = time[3] != '?' ? time[3] : '5';
+    time[4] = time[4] != '?' ? time[4] : '9';
+    return time;
+}
+
+int main()
+{
+    return 0;
+}
