8400a25c61f9fed82a502caa276fcda64cac00b2
Juntao Ren <kevinrjt@163.com>
2021-01-31 16:03:06 +0800

    Solve flatten-nested-list-iterator

--- definitions.h
+++ definitions.h
@@ -97,4 +97,41 @@ public:
     }
 };
 
+// This is the interface that allows for creating nested lists.
+// You should not implement it, or speculate about its implementation
+class NestedInteger
+{
+public:
+    NestedInteger() : _isInt(false) {}
+
+    NestedInteger(int val) : _isInt(true), _val(val) {}
+
+    NestedInteger(vector<NestedInteger> &list) : _isInt(false), _list(list) {}
+
+    // Return true if this NestedInteger holds a single integer, rather than a nested list.
+    bool isInteger() const
+    {
+        return _isInt;
+    }
+
+    // Return the single integer that this NestedInteger holds, if it holds a single integer
+    // The result is undefined if this NestedInteger holds a nested list
+    int getInteger() const
+    {
+        return _val;
+    }
+
+    // Return the nested list that this NestedInteger holds, if it holds a nested list
+    // The result is undefined if this NestedInteger holds a single integer
+    const vector<NestedInteger> &getList() const
+    {
+        return _list;
+    }
+
+private:
+    bool _isInt;
+    int _val;
+    vector<NestedInteger> _list;
+};
+
 #endif
---
+++ flatten-nested-list-iterator/flatten-nested-list-iterator.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <stack>
+#include <utility>
+#include <vector>
+
+#include "../definitions.h"
+
+using namespace std;
+
+#define SOLUTION_3
+
+#ifdef SOLUTION_1
+class NestedIterator
+{
+public:
+    NestedIterator(vector<NestedInteger> &nestedList)
+    {
+        for (auto &nestedInt : nestedList)
+        {
+            iterate(nestedInt);
+        }
+    }
+
+    int next()
+    {
+        return _vals[_index++];
+    }
+
+    bool hasNext()
+    {
+        return _index < _vals.size();
+    }
+
+private:
+    void iterate(const NestedInteger &nestedInt)
+    {
+        if (nestedInt.isInteger())
+        {
+            _vals.push_back(nestedInt.getInteger());
+        }
+        else
+        {
+            for (auto &child : nestedInt.getList())
+            {
+                iterate(child);
+            }
+        }
+    }
+
+    vector<int> _vals;
+    int _index = 0;
+};
+#endif
+
+#ifdef SOLUTION_2
+class NestedIterator
+{
+public:
+    NestedIterator(vector<NestedInteger> &nestedList)
+    {
+        _stack.emplace(&nestedList, -1);
+        advance();
+    }
+
+    int next()
+    {
+        auto p = _stack.top();
+        int val = p.first->at(p.second).getInteger();
+        advance();
+        return val;
+    }
+
+    bool hasNext()
+    {
+        return !_stack.empty();
+    }
+
+private:
+    void advance()
+    {
+        while (!_stack.empty())
+        {
+            ++_stack.top().second;
+            auto p = _stack.top();
+            if (p.second < p.first->size())
+            {
+                auto &nestedInt = p.first->at(p.second);
+                if (nestedInt.isInteger())
+                {
+                    break;
+                }
+
+                _stack.emplace(&(nestedInt.getList()), -1);
+            }
+            else
+            {
+                _stack.pop();
+            }
+        }
+    }
+
+    stack<pair<const vector<NestedInteger> *, int>> _stack;
+};
+#endif
+
+#ifdef SOLUTION_3
+class NestedIterator
+{
+public:
+    NestedIterator(vector<NestedInteger> &nestedList)
+    {
+        push(nestedList);
+    }
+
+    int next()
+    {
+        hasNext();
+        int val = _stack.top()->getInteger();
+        _stack.pop();
+        return val;
+    }
+
+    bool hasNext()
+    {
+        while (!_stack.empty() && !_stack.top()->isInteger())
+        {
+            auto nestedInt = _stack.top();
+            _stack.pop();
+            push(nestedInt->getList());
+        }
+
+        return !_stack.empty();
+    }
+
+private:
+    void push(const vector<NestedInteger> &nestedList)
+    {
+        for (auto it = nestedList.rbegin(); it != nestedList.rend(); ++it)
+        {
+            _stack.push(&(*it));
+        }
+    }
+
+    stack<const NestedInteger *> _stack;
+};
+#endif
+
+int main()
+{
+    vector<NestedInteger> nestedList{vector<NestedInteger>{1, 2}, 3, vector<NestedInteger>{vector<NestedInteger>{vector<NestedInteger>{4, 5}, 6}, 7, vector<NestedInteger>{8, 9}}};
+    NestedIterator i(nestedList);
+    while (i.hasNext())
+    {
+        cout << i.next() << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
